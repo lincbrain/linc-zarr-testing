ff4cac217977d8e9ed2abe4cb4b734f39af3c51a
Aaron Kanzer <aaronkanzer@Aarons-MacBook-Pro.local>
2024-03-06 15:15:37 -0500

    A bit more progress

--- scripts/zarr_script.py
+++ scripts/zarr_script.py
@@ -6,16 +6,25 @@ from zarr_constants import S3_ZARR_PATH, S3_BUCKET
 from git_reproducibility import get_unique_identifier
 from datetime import datetime
 
-current_time = datetime.now()
+
+def print_event(event_name):
+    current_time = datetime.now()
+    formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+    print(f"{event_name}: {formatted_time}")
+
 
 os.environ["ZARR_V3_EXPERIMENTAL_API"] = "1"
 
-first_timestamp_str = f"{current_time.strftime('%Y-%m-%d %H:%M:%S')}.{int(current_time.microsecond / 1000)}"
 client = Client()
 repo = client.get_repo("gablab/aaronkanzer")
-next_timestamp_str = f"{current_time.strftime('%Y-%m-%d %H:%M:%S')}.{int(current_time.microsecond / 1000)}"
+print_event("Get Arraylake Client")
 
-# repo.checkout(ref="zarr-test")
+repo.new_branch("aaron-branch-1")
+# repo.checkout(ref='aaron-branch-1', for_writing=True)
+# repo.checkout('65ddebeee0a8c739300e3feb', for_writing=True)
+print_event("Checkout Branch")
+
+
 zarr_group = repo.root_group
 zarr_group.attrs['title'] = "Zarr Versioning Proof of Concept"
 zarr_group.attrs['author'] = "Aaron Kanzer"
@@ -27,24 +36,36 @@ s3 = s3fs.S3FileSystem(anon=False)  # export AWS_PROFILE=mcgovern
 store = s3fs.S3Map(root=f'{S3_BUCKET}/{S3_ZARR_PATH}', s3=s3, check=False)  # <fsspec.mapping.FSMap object at 0x11da3f4f0>
 zarr_store = zarr.open(store, mode='a')   # <zarr.hierarchy.Group '/'>
 
+print_event("Checkout Zarr and Open")
+
 zarr_array_dataset = zarr_store["0"]  # <zarr.core.Array '/0' (1, 1, 2048, 2048, 17574) uint16>
+print_event(f"Get subset {zarr_array_dataset}")
+
 subset = zarr_array_dataset[0, 0, :10, :10, :10]
 old_value = subset[0, 0, 1]
-subset[0, 0, 1] = subset[0, 0, 1] + 10
-new_value = subset[0, 0, 1]
-# group_names = list(zarr_group)
-# group_name = "friday-feb-23"
-# if group_name not in group_names:
-#     new_array = zarr_group.create(
-#         group_name,
-#         shape=(1, 1, 1024, 1024, 17574),
-#         chunks=(1, 1, 256, 256, 256),
-#         dtype="uint16",
-#         fill_value=0
-#     )
+print_event(f"Old subset value: {old_value}")
 
+subset[0, 0, 1] = subset[0, 0, 1] + 20
+new_value = subset[0, 0, 1]
+print_event(f"New subset value: {new_value}")
+
+
+group_names = list(zarr_group)
+group_name = "friday-feb-25"
+if group_name not in group_names:
+    new_array = zarr_group.create(
+        group_name,
+        shape=(1, 1, 1024, 1024, 17574),
+        chunks=(1, 1, 256, 256, 256),
+        dtype="uint16",
+        fill_value=0
+    )
 
 repo.commit(f'{get_unique_identifier()} Old: {old_value}, New: {new_value}')
+
+
+
+# repo.commit(f'{get_unique_identifier()} Old: {old_value}, New: {new_value}')
 
 # Get array to alter
 # new_array = zarr_group.get("new_zarr_test")
